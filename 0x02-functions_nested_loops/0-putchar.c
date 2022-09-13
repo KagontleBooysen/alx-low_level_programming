eb422a74e8c0548273ea569ac292e3e6dc8cc555
KagontleBooysen <k.booysen@alustudent.com>
2022-09-13 12:29:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-* main - Print the word "main"
-*
-* Return: Always 0 (Success)
-*/
+ * main - print _putchar
+ * Description: It should be followed by a new line
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char h[] = "main";
-	int i;
+char text[9] = "_putchar";
+int i = 0;
 
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(h[i]);
-		}
-		_putchar('\n');
-	return (0);
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+
+return (0);
 }
