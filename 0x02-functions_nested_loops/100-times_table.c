ff3e99c620a6877e38b9ac363266f09042c47d9f
KagontleBooysen <k.booysen@alustudent.com>
2022-09-13 19:49:13 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,54 @@
 #include "main.h"
 
 /**
-  * print_times_table - prints the n times table, starting with 0.
-  * @n: The number in which the times talbe to be print.
-  */
+ * print_times_table - print times table
+ * @n: integer for which the times table will be printed
+ *
+ * Description: prints the times table
+ *
+ * Return: void
+ */
+
 void print_times_table(int n)
 {
-	int i, j, a, b, c;
+int row, column, product;
 
-	for (i = 0; i <= n; i++)
-	{
-		if (n > 15 || n < 0)
-			break;
-		for (j = 0; j <= n; j++)
-		{
-			int num =  i * j;
+if (n >= 0 && n < 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = row * column;
 
-			b = num / 10;
-			c = num % 10;
-			if (num <= 9)
-			{
-				if (j != 0)
-				{
-					_putchar(32);
-					_putchar(32);
-				}
-			}
-			else if (num > 9 && num < 100)
-			{
-				_putchar(32);
-				_putchar('0' + b);
-			}
-			else if (num >= 100)
-			{
-				_putchar('0' + (num / 100));
-				_putchar('0' + (b % 10));
-			}
-			_putchar('0' + c);
-			if (j != n)
-			{
-				_putchar(',');
-				_putchar(32);
-			}
-		}
-		_putchar('\n');
-	}
+if (column == 0)
+_putchar('0');
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(product % 10 + '0');
+}
+else if (product >= 10 && product < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
+}
+else if (product > 99 && product < 1000)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(product / 100 + '0');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
