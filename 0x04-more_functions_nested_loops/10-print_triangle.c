8a3dc8d5d16908d67dafeb26e3068f755fa3b915
KagontleBooysen <k.booysen@alustudent.com>
2022-09-15 12:43:22 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_triangle - prints a right triangle.
+  * @size: the size (hight and width) of the triangle.
+  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			j = 0;
+			while (j < (size - i))
+			{
+				_putchar(32);
+				j++;
+			}
+			j = 0;
+			while (j < i)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
