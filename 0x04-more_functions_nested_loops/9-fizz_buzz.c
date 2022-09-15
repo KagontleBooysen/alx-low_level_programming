c1990ff461d88e8917388d8de424bed0c99d1b50
KagontleBooysen <k.booysen@alustudent.com>
2022-09-15 12:37:16 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - entry point.
+  * Return: 0 on success.
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			if (i != 100)
+				printf("%s ", "Buzz");
+			else
+				printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("%c", '\n');
+	return (0);
+}
