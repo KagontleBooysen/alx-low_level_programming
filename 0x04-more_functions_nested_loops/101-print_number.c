61c3a99fcc7ebec42d77ba66208e87b8fee6ba20
KagontleBooysen <k.booysen@alustudent.com>
2022-09-15 13:07:10 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 /**
-  * print_number - prints an integer using only _putchar function.
-  * @n: The number to be printed.
-  */
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
 void print_number(int n)
 {
-	int d, k, num, tmp, a;
+unsigned int b;
 
-	d = 0;
-	k = 1;
-	tmp = n;
-	while (n / 10 != 0)
-	{
-		n = n / 10;
-		d++;
-	}
-	if (tmp < 0)
-	{
-		_putchar('-');
-		tmp = -tmp;
-	}
-	while (d >= 1)
-	{
-		for (a = 0; a < d; a++)
-			k *= 10;
-		num = tmp / k;
-		tmp = tmp % k;
-		_putchar('0' + num);
-		d--;
-		k = 1;
-	}
-	_putchar('0' + tmp);
+if (n < 0)
+{
+b = -n;
+_putchar('-');
+}
+else
+{
+b = n;
+}
+if (b / 10)
+{
+print_number(b / 10);
+}
+_putchar((b % 10) + '0');
 }
